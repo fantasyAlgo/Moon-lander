833f0ba985d2f56c9732b54d1227817b3d14e67d
raifujo <raifujo.tachi@gmail.com>
2024-04-27 22:27:30 +0200

    Replaced the random terrain generator with value noise

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+import { Simple1DNoise } from './perlin.js';
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let perlin = new Simple1DNoise();
 let camera_offset = {x: 0, y: 0};
 class Player {
     constructor(x, y, width, height, color, weight, fuel) {
@@ -70,67 +73,56 @@ class Player {
         ctx.fill('evenodd');
     }
 }
-class Terrain {
-    constructor() {
-      this.points = [];
-      for (let x = -1000; x <= canvas.width+1000; x += 200) {
-        let y = Math.random() * canvas.height / 2 + canvas.height / 2;
-        this.points.push({x, y});
-      }
-    }
+function drawTerrain(player) {
+    ctx.beginPath();
 
-    draw() {
-      ctx.beginPath();
-      ctx.moveTo(0, canvas.height);
-      for (let point of this.points)
-        ctx.lineTo(point.x+camera_offset.x, point.y+camera_offset.y);
-      ctx.lineTo(canvas.width, canvas.height);
-      ctx.closePath();
-      ctx.fillStyle = "white";
-      ctx.fill();
+    ctx.moveTo(player.x > 0 ? -player.x : -camera_offset.x, canvas.height);
+    for (let i = -camera_offset.x; i < canvas.width + player.x ; i+=10) 
+        ctx.lineTo(i+camera_offset.x, perlin.getVal(i/200)*500+camera_offset.y);
+    ctx.lineTo(canvas.width + (player.x > 0 ?  player.x : camera_offset.x), canvas.height);
+    ctx.closePath();
+    ctx.fillStyle = "white";
+    ctx.fill();
 
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
-      ctx.stroke();
-    }
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    ctx.stroke();
 }
+class Terrain {}
 
+const player = new Player(10, 1, 25, 50, "black", 50, 10000);
+player.move(0, -400);
 
-const triangle = new Player(100, 10, 25, 50, "black", 50, 1000);
-const terrain = new Terrain();
 let xRotation = 0;
-
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#ADD8E6";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    triangle.rotate(xRotation);
-    camera_offset = {x: -triangle.x + canvas.width/2, y: -triangle.y + canvas.height/2};
+    player.rotate(xRotation);
+    camera_offset = {x: -player.x + canvas.width/2, y: -player.y + canvas.height/2};
 
-    triangle.update();
-    triangle.draw();
-    terrain.draw();
+    player.update();
+    player.draw();
+    drawTerrain(player);
 
     ctx.fillStyle = "black";
     ctx.font = "20px Arial";
-    ctx.fillText("Fuel: " + triangle.fuel, 10, 30);
-    ctx.fillText("Y-velocity: " + -Math.floor(triangle.force.y*100)/100, 10, 60);
+    ctx.fillText("Fuel: " + player.fuel, 10, 30);
+    ctx.fillText("Y-velocity: " + -Math.floor(player.force.y*100)/100, 10, 60);
     requestAnimationFrame(animate);
 }
 animate();
-triangle.draw();
-
 
 document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowUp" && triangle.fuel > 0) {
-        console.log(triangle.totalRotation, Math.cos(triangle.totalRotation), Math.sin(triangle.totalRotation));
-        triangle.force.x += Math.sin(triangle.totalRotation)/90;
-        triangle.force.y -= Math.cos(triangle.totalRotation)/40;
-        triangle.fuel -= 1;
+    if (e.key === "ArrowUp" && player.fuel > 0) {
+        console.log(player.totalRotation, Math.cos(player.totalRotation), Math.sin(player.totalRotation));
+        player.force.x += Math.sin(player.totalRotation)/90;
+        player.force.y -= Math.cos(player.totalRotation)/40;
+        player.fuel -= 1;
     }
-    if (e.key === "ArrowRight") xRotation = -0.009;
-    if (e.key === "ArrowLeft") xRotation = 0.009;
+    if (e.key === "ArrowRight") xRotation = 0.009;
+    if (e.key === "ArrowLeft") xRotation = -0.009;
 });
 document.addEventListener("keyup", (e) => {
     if (e.key === "ArrowRight" || e.key === "ArrowLeft") 
