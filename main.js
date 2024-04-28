0650596aefd8ded33de269e446a1db210f85414d
raifujo <raifujo.tachi@gmail.com>
2024-04-28 21:40:41 +0200

    Added a particle system

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ class Player {
     }
     update() {
         this.move(this.force.x, this.force.y);
-        this.force.y += 0.000981*2 + clamp(this.force.y/this.weight/20, -0.000981, 0.000981);
+        this.force.y += 0.000981*2.3 + clamp(this.force.y/this.weight/20, -0.000981, 0.000981);
         this.force.x *= 0.999;
     }
     draw() {
@@ -90,22 +90,67 @@ function drawTerrain(player) {
         ctx.lineTo(i+camera_offset.x, perlin.getVal(i/200)*500+camera_offset.y);
     ctx.lineTo(canvas.width + (player.x > 0 ?  player.x : camera_offset.x), canvas.height);
     ctx.closePath();
-    ctx.fillStyle = "white";
+    ctx.fillStyle = "#121211";
     ctx.fill();
 
-    ctx.strokeStyle = "black";
+    ctx.strokeStyle = "white";
     ctx.lineWidth = 2;
     ctx.stroke();
 }
-class Terrain {}
+class RocketParticleSystem {
+    constructor() {
+        this.active_particles = [];
+    }
+    emit(start_pos, vel){
+        this.active_particles.push({
+            time_rem: 1+(Math.random()*2-1)/10,
+            pos: start_pos,
+            vel: {
+                x: vel.x + (Math.random()*2 - 1)/2,
+                y: vel.y + (Math.random()*2 - 1)/10, 
+            }
+        });
+    }
+    update(){
+        let length = this.active_particles.length;
+        for (let i = 0; i < length; i++) {
+            if (this.active_particles[i].time_rem < 0){
+                this.active_particles.shift();
+                console.log(this.active_particles)
+                i -= 1;
+                length -= 1;
+                continue;
+            }
+            this.active_particles[i].pos.x += this.active_particles[i].vel.x;
+            this.active_particles[i].pos.y += this.active_particles[i].vel.y;
+            this.active_particles[i].time_rem -= 0.01;
+        }
+    }
+    draw(){
+        let length = this.active_particles.length;
+        let pos;
+        ctx.fillStyle = "#916846";
+        for (let i = 0; i < length; i++) {
+            pos = this.active_particles[i].pos;
+            ctx.fillRect(pos.x, pos.y, 2, 2);
+        }
+    }
+}
+//class Terrain {}
 
-const player = new Player(10, 1, 25, 50, "black", 50, 10000);
+const player = new Player(10, 1, 25, 50, "#929990", 50, 4000);
 player.move(0, -400);
 
 let goUp = false;
 let xRotation = 0;
 let attractPoint = [0,0]; 
 let sameInInterval = (a, b, interval) => Math.abs(a-b) < interval;
+let particles = new RocketParticleSystem();
+
+let middlePoint = (player, camera_offset) => {
+    return {x: (player.lst[1][0]+player.lst[2][0])/2 + camera_offset.x, 
+            y: (player.lst[1][1]+player.lst[2][1])/2 + camera_offset.y};
+};
 function animate() {
     attractPoint = [perlin.getVal(player.lst[1][0]/200)*500 + camera_offset.y,
                     perlin.getVal(player.lst[2][0]/200)*500 + camera_offset.y];
@@ -117,7 +162,7 @@ function animate() {
         else window.location.href = "game.html";
     }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#ADD8E6";
+    ctx.fillStyle = "#1c1c1b";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     player.rotate(xRotation);
@@ -127,20 +172,27 @@ function animate() {
     player.draw();
     drawTerrain(player);
 
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "white";
     ctx.font = "20px Arial";
     ctx.fillText("Fuel: " + player.fuel, 10, 30);
     ctx.fillText("Y-velocity: " + -Math.floor(player.force.y*100)/100, 10, 60);
     if (goUp && player.fuel > 0) rocket_up();
     ctx.fillRect(player.lst[1][0]+camera_offset.x, attractPoint[0], 1, 40);
     ctx.fillRect(player.lst[2][0]+camera_offset.x, attractPoint[1], 1, 40);
+
+    if (goUp)
+        particles.emit({x: player.lst[3][0]+camera_offset.x, y: player.lst[3][1]+camera_offset.y}, 
+                        {x: -player.force.x, y: -player.force.y});
+    particles.update();
+    particles.draw();
+
     requestAnimationFrame(animate);
 }
 animate();
 
 let rocket_up = () =>{
     player.force.x += Math.sin(player.totalRotation)/180;
-    player.force.y -= Math.cos(player.totalRotation)/140;
+    player.force.y -= Math.cos(player.totalRotation)/160;
     player.fuel -= 1;
     goUp = true;
 }
